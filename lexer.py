aa28d5c19916d94066695e10d15a45cbd5a011e6
Nour Eldeen <nourborhan99@hotmail.com>
2021-05-05 22:11:32 +0200

    Added Lexemes
    Co-Authored-By: Ramez007 <45740184+Ramez007@users.noreply.github.com>

--- lexer.py
+++ lexer.py
@@ -6,23 +6,60 @@ class Lexer():
         self.lexer = LexerGenerator()
 
     def _add_tokens(self):
-        # Print
-        self.lexer.add('PRINT', r'print')
-        # Parenthesis
-        self.lexer.add('OPEN_PAREN', r'\(')
-        self.lexer.add('CLOSE_PAREN', r'\)')
-        # Semi Colon
-        self.lexer.add('SEMI_COLON', r'\;')
-        # Operators
+        # Arithmetic Operators
         self.lexer.add('SUM', r'\+')
-        # Number
+        self.lexer.add("MINUS", r'\-')
+        self.lexer.add("MULT", r'\*')
+        self.lexer.add("DIV", r'\/')
+
+        #Logic Operators
+        self.lexer.add("AND", r'\&&')
+        self.lexer.add("OR", r'\||')
+        self.lexer.add("NOT", r'\~')
+
+        #Relational Operators
+        self.lexer.add("EQUALS", r'\==')
+        self.lexer.add("LessThan", r'\<')
+        self.lexer.add("MoreThan", r'\>')
+        # Numbers
         self.lexer.add('NUMBER', r'\d+')
         # Ignore spaces
         self.lexer.ignore('\s+')
 
         self.lexer.add("Class", r'\Division')
         self.lexer.add("Inheritance", r'\InferedFrom')
-        self.lexer.add("Condition", r'WhetherDo-Else')
+        self.lexer.add("Condition", r'\WhetherDo')
+        self.lexer.add("ElseCondition", r'\Else')
+        self.lexer.add("Integer",r'\Ire')
+        self.lexer.add("SInteger",r'\Sire')
+        self.lexer.add("Character",r'\Clo')
+        self.lexer.add("String",r'\SetOfClo')
+        self.lexer.add("Float", r'\FBU')
+        self.lexer.add("SFloat", r'\SFBU')
+        self.lexer.add("Void", r'\NoneValue')
+        self.lexer.add("Break", r'\TerminateThisNow')
+        self.lexer.add("Loop", r'\RingWhen')
+        self.lexer.add("Return", r'\BackedValue')
+        self.lexer.add("Struct", r'\STT')
+        self.lexer.add("Switch", r'\Check-CaseOf')
+        self.lexer.add("Start Statement", r'\Beginning')
+        self.lexer.add("End Statement", r'\End')
+        self.lexer.add("Assign", r'\=')
+        self.lexer.add("Access", r'\.')
+        self.lexer.add("CurlyOpen", r'\{')
+        self.lexer.add("CurlyClose", r'\}')
+        self.lexer.add("SquareOpen", r'\[')
+        self.lexer.add("SquareClose", r'\]')
+        self.lexer.add("DoubleQuote", r'\"')
+        self.lexer.add("SingleQuote", r"\'")
+        self.lexer.add("Inclusion", r'\Using')
+        self.lexer.ignore("MultiLineComment", r'\/#[^#/]+#\/')
+        self.lexer.ignore("SingleComment", r'\/-.*')
+        self.lexer.add("TokenDelimiter", r'\@')
+        self.lexer.add("LineDelimiter", r'\;')
+        self.lexer.add("OpenBrace", r'\(')
+        self.lexer.add("CloseBrace", r'\)')
+
 
 
     def get_lexer(self):
