376ee6cd89d6dec9c3c34fbb4ebc8aed85c5ad12
Nour Eldeen <nourborhan99@hotmail.com>
2021-05-08 02:06:21 +0200

    Added tokens into a list of token stream

--- main.py
+++ main.py
@@ -7,38 +7,27 @@ Ire@X=2;
 FBU@Y=2.2;
 """
 
-
-
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
 lines = tokens.s.splitlines()
 
-# LineNumber = 0
-# TokenNumber = 1
-# for line in lines:
-#     LineTokens = lexer.lex(line)
-#     for token in LineTokens:
-#         tokenStream = {
-#             "Line No": LineNumber,
-#             "Lexeme": token.value,
-#             "Return Token": token.name,
-#             "Lexeme NO in line": TokenNumber,
-#             "Matchability": "Matched"
-#         }
-#         print(tokenStream)
-#         TokenNumber += 1
-#     LineNumber += 1
-#     TokenNumber = 1
+TokenStream = []
+LineNumber = 0
+TokenNumber = 1
+for line in lines:
+    LineTokens = lexer.lex(line)
+    for token in LineTokens:
+        SingleToken = {
+            "Line No": LineNumber,
+            "Lexeme": token.value,
+            "Return Token": token.name,
+            "Lexeme NO in line": TokenNumber,
+            "Matchability": "Matched"
+        }
+        TokenStream.append(SingleToken)
+        TokenNumber += 1
+    LineNumber += 1
+    TokenNumber = 1
 
-
-    # print(token.getstr())
-
-
-# for token in tokens:
-#     print(token)
-
-
-# pg = Parser()
-# pg.parse()
-# parser = pg.get_parser()
-# parser.parse(tokens).eval()
+for obj in TokenStream:
+    print(obj)
